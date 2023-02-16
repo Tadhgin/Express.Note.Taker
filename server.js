bff1cde662b67e5703bb494bb5cc85fc12c8d6cf
Tadhgin_White <teddy_bear_morbid@hotmail.com>
2023-02-16 21:21:48 +1100

    update

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apis = require('./routes/apiRoutes');
+const html = require('./routes/htmlRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api', apis);
+app.use('/', html);
 
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
