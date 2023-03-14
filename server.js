516de0d2cecfc823d70e440f2620538f645d3108
Tadhgin_White <teddy_bear_morbid@hotmail.com>
2023-03-15 00:01:46 +1100

    re-arranged files

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express');
+// Require Dependencies
+const express = require("express");
+const fs = require("fs");
 const path = require('path');
+
+// Initialize express app
 const app = express();
-const port = process.env.PORT || 3001;
-const apis = require('./routes/apiRoutes');
-const html = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+// Setup data parsing
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
 
-app.use(express.static('public'));
+//Require routes file
+require('./routes/routes')(app);
 
-app.use('/api', apis);
-app.use('/', html);
-
-app.listen(port, () =>
-    console.log(`App listening at http://localhost:${port}`)
-);
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT); //http://localhost:3000
+});  
