50f89cd8f6a00207368e1df8cd0d424263f21cc9
Tadhgin_White <teddy_bear_morbid@hotmail.com>
2023-04-20 19:31:02 +1000

    filePaths

--- server.js
+++ server.js
@@ -2,17 +2,9 @@
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
-const dotenv = require('dotenv');
-
-//Read the contents of the .env file
-const envContents = fs.readFileSync('.env', 'utf8');
-
-//Parse the contents of the .env file into an object
-const envConfig = dotenv.parse(envContents);
 
 // Initialize express app
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Setup data parsing
 app.use(express.urlencoded({ extended: true }));
